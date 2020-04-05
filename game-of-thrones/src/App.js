194f75b66eb07e88a4e52fec3df85151a3efbeed
Student_M9 <Student_M9@PS-DAL-M9.local>
2020-04-05 17:52:23 -0500

    preparing for all api calls

--- game-of-thrones/src/App.js
+++ game-of-thrones/src/App.js
@@ -1,5 +1,23 @@
 import React from 'react';
 import './App.css';
+import axios from "axios";
+
+let one = 'http://anapioficeandfire.com/api/characters/16';
+let two = '';
+let three = '';
+let four = '';
+let five = '';
+let six = '';
+let seven = '';
+
+const requestOne = axios.get(one);
+const requestTwo = axios.get(two);
+const requestThree = axios.get(three);
+const requestFour = axios.get(four);
+const requestFive = axios.get(five);
+const requestSix = axios.get(six);
+const requestSeven = axios.get(seven);
+
 
 function App() {
   return (
