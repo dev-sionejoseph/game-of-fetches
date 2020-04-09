ee1983d790f97307c6033ec1c3806f25e9328850
Student_M9 <Student_M9@PS-DAL-M9.local>
2020-04-09 11:41:42 -0500

    finally fixed the call and compile errors

--- game-of-thrones/src/App.js
+++ game-of-thrones/src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import axios from "axios";
-
-let one = 'https://anapioficeandfire.com/api/characters/16';
+  
+let one = 'https://www.anapioficeandfire.com/api/characters/16';
 let two = 'https://www.anapioficeandfire.com/api/houses/378';
 let three = 'https://www.anapioficeandfire.com/api/houses/229';
 let four = 'https://www.anapioficeandfire.com/api/houses/17';
 let five = 'https://www.anapioficeandfire.com/api/characters/901';
 let six = 'https://www.anapioficeandfire.com/api/houses/362';
 let seven = 'https://www.anapioficeandfire.com/api/characters/232';
-
+  
 const requestOne = axios.get(one);
 const requestTwo = axios.get(two);
 const requestThree = axios.get(three);
@@ -19,33 +19,47 @@ const requestSix = axios.get(six);
 const requestSeven = axios.get(seven);
 
 
-function App() {
+class App extends Component {
   
-  axios
-  .all([requestOne, requestTwo, requestThree, requestFour, requestFive, requestSix, requestSeven])
-  .then(
-    axios.spread((...responses) => {
-      const responseOne = responses[0];
-      const responseTwo = responses[1];
-      const responseThree = responses[2];
-      const responseFour = responses[3];
-      const responseFive = responses[4];
-      const responseSix = responses[5];
-      const responseSeven = responses[6];
+  constructor(props) {
+    super(props);
 
-      
-      console.log(responseOne, responseTwo, responseThree, responseFour, responseFive, responseSix, responseSeven);
-    })
-  )
-  .catch(errors => {
-    console.error(errors);
-  });
+    this.state = {
+      isLoaded: null,
+    }
+  }
+
+  componentDidMount(){
+    axios
+    .all([requestOne, requestTwo, requestThree, requestFour, requestFive, requestSix, requestSeven])
+    .then(
+      axios.spread((...responses) => {
+        const responseOne = responses[0].data;
+        const responseTwo = responses[1].date;
+        const responseThree = responses[2].data;
+        const responseFour = responses[3].data;
+        const responseFive = responses[4].data;
+        const responseSix = responses[5].data;
+        const responseSeven = responses[6].data;
   
-  return (
-    <div className="App">
-      
-    </div>
-  );
+        this.setState({isLoaded:true})
+
+        console.log(responseOne, responseTwo, responseThree, responseFour, responseFive, responseSix, responseSeven);
+      })
+    )
+    .catch(errors => {
+      console.error(errors);
+    });
+  }
+  
+  
+  render(){
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
 }
 
 export default App;
